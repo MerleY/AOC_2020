66a62056a4bf08282140150ad474b37277c83ed1
ymerle <ymerle@knitiv.net>
2020-12-10 11:24:56 +0100

    day10 part 2

--- day10-2.go
+++ day10-2.go
@@ -12,124 +12,26 @@ import (
 
 func main() {
     start := time.Now()
-    lines := getFileContent("inputs/input10.txt")
+    lines := getFileContent("input10.txt")
     input := transform(lines)
     sort.Ints(input)
 
-    jolt := 0
-    three_joltDelta := 1
-    one_joltDelta := 0
-    possArrang := make([]int, len(input), 0)
+    possArrang := make([]int, len(input))
+    for i := 0; i <= 3; i++ {
+        if input[i] <= 3{
+            possArrang[i] = 1
+        }
+    }
+
     for i := 0; i < len(input); i++ {
-        arr := -1
         for j := 1; j <= 3; j++ {
-            if input[i + j] <= input[i] + 3{
-                arr++
-            }
-        }
-        for j := 1; j <= 3; j++ {
-            if input[i + j] <= input[i] + 3{
-                possArrang[j] = possArrang[i] + poss
+            if i + j < len(input) {
+                if input[i + j] <= input[i] + 3 {
+                    possArrang[i + j] += possArrang[i]
+                }
             }
         }
     }
-    fmt.Println(one_joltDelta * three_joltDelta)
+    fmt.Println(possArrang[len(possArrang) - 1])
     fmt.Println("Execution duration: " + time.Now().Sub(start).String())
-}
-
-func findConsecutivSum(table []int, ind int) []int{
-    for start := 0; start < ind; start++ {
-        sum := table[start]
-        for i := start + 1; i < ind; i++ {
-            sum += table[i]
-            if sum > table[ind] {
-                break
-            }
-            if sum == table[ind] {
-                return table[start:i]
-            }
-        }
-    }
-    return nil
-}
-
-func sumOfTwoBefore(table []int, sum int) bool {
-    if (len(table) != 25) {
-        log.Fatal("Table length invalid")
-    }
-    for i, a := range table {
-        for _, b := range table[i:] {
-            if a + b == sum {
-                return true
-            }
-        }
-    }
-
-    return false
-}
-
-func intInSlice(list []int, a int) bool {
-    for _, b := range list {
-        if b == a {
-            return true
-        }
-    }
-    return false
-}
-
-func min(list []int) int{
-    var min int
-    for i, b := range list {
-        if i == 0 {
-            min = b
-        } else if (b < min) {
-            min = b
-        }
-    }
-    return min
-}
-
-func max(list []int) int{
-    var max int
-    for i, b := range list {
-        if i == 0 {
-            max = b
-        } else if (b > max) {
-            max = b
-        }
-    }
-    return max
-}
-
-func transform (lines []string) []int {
-    var numbers []int
-    for i, line := range lines {
-        n, err := strconv.Atoi(line)
-        if err != nil {
-            log.Fatal("Could not convert line " + string(i))
-        }
-        numbers = append(numbers, n)
-    }
-
-    return numbers
-}
-
-func getFileContent(name string) []string {
-    var lines []string
-    file, err := os.Open(name)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer func() {
-        if err = file.Close(); err != nil {
-            log.Fatal(err)
-        }
-    }()
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        input := scanner.Text()
-        lines = append(lines, input)
-    }
-
-    return lines
 }
