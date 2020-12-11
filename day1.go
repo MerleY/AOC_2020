c9724d151d62ce2ab82a5c174a2bf80a74d6e367
M-Yoann <yoann.merle@protonmail.com>
2020-12-11 19:50:34 +0100

    Major refactoring

--- day1.go
+++ day1.go
@@ -1,35 +1,27 @@
 package main
 
 import "fmt"
-import "os"
-import "bufio"
-import "log"
-import "strconv"
+import "aoc/input"
 
 func main() {
-    file, err := os.Open("input1-1.txt")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer func() {
-        if err = file.Close(); err != nil {
-            log.Fatal(err)
-        }
-    }()
-    var input []int
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        i, _ := strconv.Atoi(scanner.Text())
-        input = append(input, i)
-    }
-
-    for i, n := range input {
-        for j, n2 := range input[i:] {
-            for _, n3 := range input[j:] {
-                if n + n2 + n3 == 2020 {
-                    fmt.Println(n2 * n *n3)
-                }
+    input := input.Load(1).ToIntArray()
+    // part 1
+	for i, n := range input {
+		for _, n2 := range input[i:] {
+			if n + n2 == 2020 {
+                fmt.Printf("Star 1 : %v\n", n * n2)
             }
         }
     }
+
+    // part 2
+	for i, n := range input {
+		for j, n2 := range input[i:] {
+			for _, n3 := range input[j:] {
+				if n+n2+n3 == 2020 {
+                    fmt.Printf("Star 2 : %v\n", n * n2 * n3)
+				}
+			}
+		}
+	}
 }
