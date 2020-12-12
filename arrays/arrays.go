5eae426a6199e3ce83b023910309c398559e14bf
M-Yoann <yoann.merle@protonmail.com>
2020-12-12 17:05:37 +0100

    Refactor day 5

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -9,3 +9,24 @@ func StringIn(a string, list []string) bool {
 
     return false
 }
+
+func IntIn(a int, list []int) bool {
+    for _, b := range list {
+        if b == a {
+            return true
+        }
+    }
+    return false
+}
+
+func IntMax(list []int) int {
+    var max int
+    for i, b := range list {
+        if i == 0 {
+            max = b
+        } else if b > max {
+            max = b
+        }
+    }
+    return max
+}
---
+++ day05.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    "fmt"
+    "math"
+    "log"
+    "aoc/input"
+    "aoc/arrays"
+)
+
+
+func main() {
+    inputs := input.Load(5).ToStringArray()
+
+    // part 1
+    var passportsId []int
+    var id int
+    for _, input := range inputs {
+        if len(input) != 10 {
+            log.Fatal("line with wrong length", len(input))
+        }
+
+        id = 0
+        for i, letter := range input {
+            if letter == 'B' {
+                id += int(math.Pow(2, float64(9 - i)))
+            }
+        }
+        passportsId = append(passportsId, id)
+    }
+    max := arrays.IntMax(passportsId)
+    fmt.Printf("Star 1 : %v\n", max)
+
+    // part 2
+    var seat int
+    for i := 7; i < max; i++ {
+        if !arrays.IntIn(i, passportsId) {
+            seat = i
+        }
+    }
+    fmt.Printf("Star 2 : %v\n", seat)
+}
+
